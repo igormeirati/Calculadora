361248951386be532081f714ca4c6bc8d8757cad
Igor <igormeira3@gmail.com>
2023-10-01 21:09:31 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,34 @@
+def soma(a, b):
+    return a + b
+def subtracao(a, b):
+    return a - b
+def multiplicacao(a, b):
+    return a * b
+def divisao(a, b):
+    return a / b
+
+
+def calculadora():
+    print('Selecione uma operacao:')
+    print('1. Soma')
+    print('2. Subtracao')
+    print('3. Multiplicacao')
+    print('4. Divisao')
+
+    escolha = input('Digite o numero da operacao desejada:')
+
+    num1 = float(input('Digite um numero: '))
+    num2 = float(input('Digite outeo numero: '))
+
+    if escolha == '1':
+        print('Resultado: ', soma(num1, num2))
+    elif escolha == '2':
+        print('Resultado: ', subtracao(num1, num2))
+    elif escolha == '3':
+        print('Resultado: ', multiplicacao(num1, num2))
+    elif escolha == '4':
+        print('Resultado: ', divisao(num1, num2))
+    else:
+        print('Opcao Invalida!')
+    
+calculadora()
---
+++ redme.md
@@ -0,0 +1,3 @@
+Calculadora feita com ajuda do ChatGPT para induzir minha pratica na digitacao de codigos e ajudar a entender a logica de programacao usando python.
+
+Neste codigo por exemplo aprendi o uso do if / else, funcoes muito importantes no dia a dia de qualquer liguagem.
